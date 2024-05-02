d0ea222ca54fa209a37e07419989a134b8f6a799
Subham Agarwal <agarwalsubham79@gmail.com>
2024-05-02 11:26:02 +0530

    added properties in model class review

--- ReviewService/ReviewService/src/main/java/com/UberReviewService/ReviewService/Models/Review.java
+++ ReviewService/ReviewService/src/main/java/com/UberReviewService/ReviewService/Models/Review.java
@@ -1,13 +1,31 @@
 package com.UberReviewService.ReviewService.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name="bookingreview")
 public class Review {
 
     @Id
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @Column(nullable = false)
+    String content;
+
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+            @CreatedDate //only handles for the object created for first time
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate //keeps track of last modification
+    Date updatedAt;
 }
