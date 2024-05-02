05ad83cdf84619fcb0dd93fc6ddb5c1b80c8aa96
Subham Agarwal <agarwalsubham79@gmail.com>
2024-05-02 11:31:41 +0530

    added access modifiers

--- ReviewService/ReviewService/src/main/java/com/UberReviewService/ReviewService/Models/Review.java
+++ ReviewService/ReviewService/src/main/java/com/UberReviewService/ReviewService/Models/Review.java
@@ -1,31 +1,36 @@
 package com.UberReviewService.ReviewService.Models;
 
 import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.util.Date;
-
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="bookingreview")
 public class Review {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
     @Column(nullable = false)
-    String content;
+    private String content;
 
-    Double rating;
+    private Double rating;
 
     @Column(nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
             @CreatedDate //only handles for the object created for first time
-    Date createdAt;
+    private Date createdAt;
 
     @Column(nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
     @LastModifiedDate //keeps track of last modification
-    Date updatedAt;
+    private Date updatedAt;
 }
