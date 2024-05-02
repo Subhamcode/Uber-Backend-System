1894130a29d28c7a9a5434b908b1bd29104daf5e
Subham Agarwal <agarwalsubham79@gmail.com>
2024-05-02 10:10:26 +0530

    added model

---
+++ ReviewService/ReviewService/src/main/java/com/UberReviewService/ReviewService/Models/Review.java
@@ -0,0 +1,13 @@
+package com.UberReviewService.ReviewService.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Review {
+
+    @Id
+
+    Long id;
+
+}
